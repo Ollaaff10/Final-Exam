7af7d379c328e46d1d768192de11fa31e22f08fb
unknown <salmansiddiquebhuiyan@gmail.com>
2023-12-13 11:05:10 +0600

    Your commit message

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-document.getElementById('search-btn').addEventListener('click', function() {
-    let city = document.getElementById('location-input').value;
-    getWeather(city);
-});
+const apiKey = "298eb75776a89f3bd444521f7d443053";
 
-function getWeather(city) {
-    const apiKey = 'YOUR_API_KEY'; // Replace with your API key
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function getWeather() {
+    const location = document.getElementById("location-input").value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
 
     fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            displayWeather(data);
-        })
-        .catch(() => {
-            alert('City not found. Please try again.');
-        });
+    .then(response => response.json())
+    .then(data => {
+        displayWeather(data);
+    })
+    .catch(error => {
+        console.error("Error fetching data: ", error);
+    });
 }
 
 function displayWeather(data) {
-    const temperature = data.main.temp;
-    const description = data.weather[0].description;
-    const resultDiv = document.getElementById('weather-result');
-    resultDiv.innerHTML = `Temperature: ${temperature}°C <br> Description: ${description}`;
+    const weatherContainer = document.getElementById("weather-container");
+    weatherContainer.innerHTML = `
+        <h2>${data.name}</h2>
+        <p>Temperature: ${data.main.temp} °C</p>
+        <p>Humidity: ${data.main.humidity}%</p>
+        <p>Weather: ${data.weather[0].main}</p>
+        <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" />
+    `;
 }
